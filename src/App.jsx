261a816cf0a7aee1e3ba37e8b139744805b27fd5
pranavkumar <pranavisngh9471@gmail.com>
2025-07-08 20:39:05 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar.jsx";
+import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import ProductDetail from "./pages/ProductDetail";
