f68010d7cb09fcf50c52f60ec25afc5252c1a4c5
pranavkumar <pranavisngh9471@gmail.com>
2025-07-09 13:09:04 +0530

    logo update

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-// export const BASE_URL = "https://anritvox-backend.onrender.com";
+// export const BASE_URL = "http://localhost:5000";
 export const BASE_URL = import.meta.env.VITE_API_URL;
 
 // Helper: attach authorization header
