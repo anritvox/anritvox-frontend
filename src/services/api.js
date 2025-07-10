fa67e2c952d8f40dcc2a0b9165e15d1cd9048d69
pranavkumar <pranavisngh9471@gmail.com>
2025-07-10 17:28:47 +0530

    added notication admin and registration

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:5000";
-// export const BASE_URL = import.meta.env.VITE_API_URL;
+// export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = import.meta.env.VITE_API_URL;
 
 // Helper: attach authorization header
 function authHeader(token) {
