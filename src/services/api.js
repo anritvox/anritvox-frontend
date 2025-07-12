6489e90c96ed8db8aa0ade3b48a48d690dd0a4bc
pranavkumar <pranavisngh9471@gmail.com>
2025-07-12 20:12:19 +0530

    env

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 // export const BASE_URL = "http://localhost:5000";
-export const BASE_URL = import.meta.env.VITE_API_URL;
+export const BASE_URL =
+  import.meta.env.VITE_API_URL || "https://api.anritvox.com";
 
 // Helper: attach authorization header
 function authHeader(token) {
