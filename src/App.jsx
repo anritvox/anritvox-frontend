ab1c5b17e6572ba5812e457fad8cc249cf100486
anritvox <anritvox@gmail.com>
2026-03-10 01:08:05 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,44 @@
-import React from \"react\";
-import { BrowserRouter, Routes, Route, useLocation } from \"react-router-dom\";
-import Navbar from \"./components/Navbar\";
-import Footer from \"./components/Footer\";
-import Home from \"./pages/Home\";
-import Shop from \"./pages/Shop\";
-import ProductDetail from \"./pages/ProductDetail\";
-import EWarranty from \"./pages/EWarranty\";
-import Contact from \"./pages/Contact\";
-import AdminLogin from \"./pages/AdminLogin\";
-import AdminDashboard from \"./pages/AdminDashboard\";
-import \"./index.css\";
+import React from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import ProductDetail from "./pages/ProductDetail";
+import EWarranty from "./pages/EWarranty";
+import Contact from "./pages/Contact";
+import AdminLogin from "./pages/AdminLogin";
+import AdminDashboard from "./pages/AdminDashboard";
+import "./index.css";
 
 function AppContent() {
-  const location = useLocation();
-  const isAdminPath = location.pathname.startsWith(\"/admin\");
-
-  return (
-    <>
-      {!isAdminPath && <Navbar />}
-      <main>
-        <Routes>
-          <Route path=\"/\" element={<Home />} />
-          <Route path=\"/shop\" element={<Shop />} />
-          <Route path=\"/shop/:id\" element={<ProductDetail />} />
-          <Route path=\"/ewarranty\" element={<EWarranty />} />
-          <Route path=\"/contact\" element={<Contact />} />
-          <Route path=\"/admin/login\" element={<AdminLogin />} />
-          <Route path=\"/admin/dashboard\" element={<AdminDashboard />} />
-        </Routes>
-      </main>
-      {!isAdminPath && <Footer />}
-    </>
-  );
+ const location = useLocation();
+ const isAdminPath = location.pathname.startsWith("/admin");
+ return (
+ <>
+ {!isAdminPath && <Navbar />}
+ <main>
+ <Routes>
+ <Route path="/" element={<Home />} />
+ <Route path="/shop" element={<Shop />} />
+ <Route path="/shop/:id" element={<ProductDetail />} />
+ <Route path="/ewarranty" element={<EWarranty />} />
+ <Route path="/contact" element={<Contact />} />
+ <Route path="/admin/login" element={<AdminLogin />} />
+ <Route path="/admin/dashboard" element={<AdminDashboard />} />
+ </Routes>
+ </main>
+ {!isAdminPath && <Footer />}
+ </>
+ );
 }
 
 function App() {
-  return (
-    <BrowserRouter>
-      <AppContent />
-    </BrowserRouter>
-  );
+ return (
+ <BrowserRouter>
+ <AppContent />
+ </BrowserRouter>
+ );
 }
 
 export default App;
