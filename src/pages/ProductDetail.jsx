f69798f5a847fc8b9463bc5ea44dccd7e1cd9a4c
anritvox <anritvox@gmail.com>
2026-04-14 15:39:09 +0530

    Fix ProductDetail - correct React import (remove extra 'ct')

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,4 +1,4 @@
-import Reactt, { useState, useEffect } from 'react';
+import React,  { useState, useEffect } from 'react';
 import QASection from '../components/QASection';
 import { useParams, Link, useNavigate } from 'react-router-dom';
 import { 
