f0fd16252a6beacfc9ffb611a280a3380e051cf3
anritvox <anritvox@gmail.com>
2026-04-19 11:58:29 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 import "./index.css";
 import App from "./App.jsx";
 import { SettingsProvider } from "./context/SettingsContext";
 
+const reCaptchaKey = import.meta.env.VITE_RECAPTCHA_SITE_KEY;
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <SettingsProvider>
-      <App />
-    </SettingsProvider>
+    <GoogleReCaptchaProvider reCaptchaKey={reCaptchaKey}>
+      <SettingsProvider>
+        <App />
+      </SettingsProvider>
+    </GoogleReCaptchaProvider>
   </StrictMode>
 );
