ae78df30236e00246aeac0004f0a704cafd07e4a
anritvox <anritvox@gmail.com>
2026-04-24 19:05:55 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,14 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 import "./index.css";
 import App from "./App.jsx";
 import { SettingsProvider } from "./context/SettingsContext.jsx";
 
-const reCaptchaKey = import.meta.env.VITE_RECAPTCHA_SITE_KEY || "fallback-key-to-prevent-crash";
-
+// 100% Clean Entry Point - Purged Conflicting ReCaptcha Provider
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GoogleReCaptchaProvider reCaptchaKey={reCaptchaKey}>
-      <SettingsProvider>
-        <App />
-      </SettingsProvider>
-    </GoogleReCaptchaProvider>
+    <SettingsProvider>
+      <App />
+    </SettingsProvider>
   </StrictMode>
 );
